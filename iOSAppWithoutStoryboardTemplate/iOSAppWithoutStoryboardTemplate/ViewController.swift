1ac18b77d23b87f3975b1c37a3517cd9704cfde2
Vivian Phung <dev+github@vivianphung.com>
2022-09-14 12:43:12 -0400

    init

---
+++ iOSAppWithoutStoryboardTemplate/iOSAppWithoutStoryboardTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAppWithoutStoryboardTemplate
+//
+//  Created by Vivian Phung on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
